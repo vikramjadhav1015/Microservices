8ad32517b1b02dd04ae3a0b364e1e48b81cf0611
Vikram Jadhav <vikramjadhav1015@gmail.com>
2022-12-11 23:22:22 +0530

    Automapper configuration

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+
+                //config.CreateMap<Product, ProductDto>().ReverseMap();
+            });
+
+            return mapperConfig;
+        }
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +13,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//For Automapper configuraiton
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
